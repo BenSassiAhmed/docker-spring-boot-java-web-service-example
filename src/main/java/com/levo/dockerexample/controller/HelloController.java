50bb644605c89e872b4875023e47ec2a25112bb1
BenSassiAhmed <52077564+BenSassiAhmed@users.noreply.github.com>
2024-08-05 12:15:01 +0100

    Update HelloController.java to old version

--- src/main/java/com/levo/dockerexample/controller/HelloController.java
+++ src/main/java/com/levo/dockerexample/controller/HelloController.java
@@ -1,19 +1,19 @@
-package com.levo.dockerexample;
+package com.levo.dockerexample.controller;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import java.util.Date;
 
-@SpringBootApplication
-public class DockerApp extends SpringBootServletInitializer {
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(DockerApp.class);
-    }
+@RestController
+@RequestMapping("docker-java-app")
+public class HelloController {
+	
+	@RequestMapping(value = "/test", method = RequestMethod.GET)
+	public String test() {
+		return "docker-java-app is up and running: " + new Date();
+	}
 
-    public static void main(String[] args) {
-        SpringApplication.run(DockerApp.class, args);
-    }
 }
+
